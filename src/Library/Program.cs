71429de82a8aa37eb07b236450799d0147143d12
axelferreira100 <GUSTAVO.FERREIRA@correo.ucu.edu.uy>
2025-09-16 22:34:40 -0300

    Challenge completed, Collatz's sequence succesfully implemented.

--- src/Library/Program.cs
+++ src/Library/Program.cs
@@ -4,7 +4,23 @@
     {
         public static int Collatz(int num)
         {
-            return 0;
+            int stepsCounter = 0;
+
+            while (num != 1)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                }
+                else
+                {
+                    num = (num * 3) + 1;
+                }
+
+                stepsCounter++;
+            }
+
+            return stepsCounter;
         }
     }
 }
